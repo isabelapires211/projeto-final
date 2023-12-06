16ddbf7763f1bf1d1dbeea5a846cf1480c95228b
Lolozix <lorranasilvadamasceno@gmail.com>
2023-12-06 11:25:47 -0400

    commit
    Arrumando o Cadastrar

---
+++ src/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Menu from "./Componentes/Menu";
+import Destaque from ".Componentes/Destaque";
+export default function Home (){
+     return(
+          <div>
+               <i> <h1>PodPah</h1> </i>
+               <Menu/>
+               <Destaque/>
+          </div>
+     );
+}
+
