6c22cadb84e81b26bc6b6ce0649f31206a8f242c
Lolozix <lorranasilvadamasceno@gmail.com>
2023-12-06 22:04:10 -0400

    commit

--- src/Cadastrar/Cadastrar.jsx
+++ src/Cadastrar/Cadastrar.jsx
@@ -1,26 +1,77 @@
 import React, { useState, useEffect } from "react";
 
 export default function Cadastrar() {
-  const [titulo, setTitulo] = useState("")
-  const [descricao, setDescricao] = useState("")
-  const [canal, setCanal] = useState("")
-  const [playlist, setPlaylist] = useState("")
-  const [data, setData] = useState("")
-  const [id, setId] = useState("")
-}
+  const [titulo, setTitulo] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [canal, setCanal] = useState("");
+  const [playlist, setPlaylist] = useState("");
+  const [data, setData] = useState("");
+  const [id, setId] = useState(1); 
 
-useEffect(() => { localStorage.setItem("Lista", JSON.stringify(lista)) }, [lista])
+  const [lista, setLista] = useState([]);
 
-const salvar = () => {
-    e.preventDefault()
-    setLista([...lista, {
+  useEffect(() => {
+    localStorage.setItem("Lista", JSON.stringify(lista));
+  }, [lista]);
+
+  const salvar = (e) => {
+    e.preventDefault();
+
+    if (!titulo || !descricao || !canal || !playlist || !data) {
+      alert("Por favor, preencha todos os campos obrigatórios.");
+      return;
+    }
+
+    setLista([
+      ...lista,
+      {
         titulo: titulo,
         descricao: descricao,
         canal: canal,
         playlist: playlist,
         data: data,
         id: id,
-      }])
+      },
+    ]);
 
-    }
+    setTitulo("");
+    setDescricao("");
+    setCanal("");
+    setPlaylist("");
+    setData("");
+    setId(id + 1); 
+  };
 
+  return (
+    <div>
+      <form onSubmit={salvar}>
+        <label>
+          Título:
+          <input type="text" value={titulo} onChange={(e) => setTitulo(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Descrição:
+          <textarea value={descricao} onChange={(e) => setDescricao(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Canal:
+          <input type="text" value={canal} onChange={(e) => setCanal(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Playlist:
+          <input type="text" value={playlist} onChange={(e) => setPlaylist(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Data:
+          <input type="text" value={data} onChange={(e) => setData(e.target.value)} />
+        </label>
+        <br />
+        <button type="submit">Salvar</button>
+      </form>
+    </div>
+  );
+}
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,29 @@
-export default function Home(){
+import React from 'react';
+import Cadastrar from '../Cadastrar/Cadastrar';
 
-     return(
-          <div>
-               <h1>Sexo</h1>
-          </div>    
-     )
+const Home = () => {
+  const videoId = [
+    '',
+    '',
+  ];
 
-}
+  return (
+    <div className="home">
+      {videoId.map((videoId, index) => (
+        <div key={index} className="video-container">
+          <iframe
+            title={`Video ${index + 1}`}
+            width="560"
+            height="315"
+            src={"https://www.youtube.com/watch?v=3Uv8FfLMhsw${videoId}"}
+           frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </div>
+      ))}
+      <Cadastrar />
+    </div>
+  );
+};
+
+export default Home;
