c69125e2831d5097340dd49c405ac0434fe6f64f
Lolozix <lorranasilvadamasceno@gmail.com>
2023-12-08 00:28:08 -0300

    commit

--- src/Detalhe/Detalhe.jsx
+++ src/Detalhe/Detalhe.jsx
@@ -1,5 +1,23 @@
+import { useParams } from "react-router-dom";
+import Card from '../Componentes/Card'
 export default function Detalhe() {
-    return (
-        <h1>Destaque</h1>
-    );
-} 
+
+            const { id } = useParams();
+            const lista = JSON.parse(localStorage.getItem("Lista"));
+        
+            const atividade = lista.filter((videos)=>{
+            if(videos.id == id){
+            return videos;
+            }
+            return null;
+            }); 
+        
+            console.log(atividade[0]);
+        
+            return(
+            <div>
+            <Card videos ={atividade[0]}/>
+            </div>
+            );  
+        
+        }
